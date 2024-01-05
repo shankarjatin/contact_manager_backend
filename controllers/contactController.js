ebf5f707a005dd231f572b40d14aee07a4e4cef3
shankarjatin <shankarjatin1005@gmail.com>
2024-01-05 18:15:24 +0530

    data created

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,18 +1,26 @@
 const asyncHandler = require("express-async-handler")
+const Contact = require("../model/contactModel");
 
 const create_contact =asyncHandler(async(req, res) => {
-    console.log("The request body is :", req.body); I
+    console.log("The request body is :", req.body);
     const { name, email, phone} = req.body;
     if (!name || !email || !phone) {
     res.status (400);
     throw new Error("All fields are mandatory !");
     }
-    res.status (201).json ({ message: "Create Contact" });
+    const contact = await Contact.create({
+    name,
+    email,
+    phone,
+    });
+    res.status (201).json(contact);
     })
 
-const get_contacts = asyncHandler(async(req,res)=>{
-    res.status(200).json({message:"hello all contacts"})
-})
+//
+const get_contacts = asyncHandler(async (req, res) => {
+    const contacts = await Contact.find();
+    res.status (200).json (contacts);
+    });
 
 const get_contact = asyncHandler( async(req,res)=>{
     res.status(200).json({message:`get individual contact ${req.param.id}`})
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config()
 const app = express()
 const port =process.env.PORT || 5000 ;
- 
+ connectDb();
 app.use(express.json());
 app.use(errorHandler)
 app.use("/api/contacts", require("./routes/contactRoutes"))
