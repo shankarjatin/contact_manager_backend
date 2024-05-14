2c62f3784f6dcd9c720416c00b140adb56bcf3aa
shankarjatin <shankarjatin1005@gmail.com>
2024-05-14 10:55:40 +0530

    cron job added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
+const cron = require("node-cron")
 const dotenv = require("dotenv").config()
 const app = express()
 const port =process.env.PORT || 5000 ;
@@ -10,6 +11,10 @@ app.use(errorHandler)
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
+
+cron.schedule('*/10 * * * * *', () => {
+    console.log('This message will be printed to the console every 10 seconds');
+  });
 
 app.listen(port ,()=>{
     console.log(`hello to ${port}`)
