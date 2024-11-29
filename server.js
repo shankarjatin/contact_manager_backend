6f3727fc4431cc69da756f0d1bbdb072d9a000c7
shankarjatin <shankarjatin1005@gmail.com>
2024-11-30 03:43:38 +0530

    server nodecron

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
 const cors = require('cors');
 var cron = require('node-cron');
+const { default: axios } = require("axios");
 const dotenv = require("dotenv").config()
 const app = express()
 const port =process.env.PORT || 5000 ;
@@ -12,12 +13,20 @@ app.use(cors());
 app.use(errorHandler)
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
+app.get('/', (req, res) => {
+  res.send('Server is running and active!');
+});
 
-
-cron.schedule('*/10 * * * * *', () => {
-    console.log('This message will be printed to the console every 10 seconds');
-  });
-
+cron.schedule('**/20 * * * * *', async () => {
+  try {
+    console.log('Pinging server to keep it awake...');
+    // Change this to your actual server's public URL
+    await axios.get('https://contact-manager-backend-2.onrender.com/');
+    console.log('Server pinged successfully');
+  } catch (error) {
+    console.error('Error pinging the server:', error.message);
+  }
+});
 app.listen(port ,()=>{
     console.log(`hello to ${port}`)
 })
